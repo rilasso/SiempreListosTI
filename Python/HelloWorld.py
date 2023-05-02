a88655b5270a65d6a5c1ab6317a5c97d90eface3
Ricardo <rilasso@bgeneral.com>
2023-05-02 16:30:44 -0500

    Pruebas automatizadas

---
+++ Python/HelloWorld.py
@@ -0,0 +1,16 @@
+import time
+import math
+def main(name):
+    i = 0 
+    arr = []
+    print (f' Hola{name}!')
+    while i < math.pi:
+        print(i)
+       i +=3.141592
+    
+
+
+
+
+if __name__ == "__main__":
+    main(", Aprende a Programar en Python")
---
+++ Python/reloj.py
@@ -0,0 +1,43 @@
+import time
+import os
+
+def main():
+    times()
+    
+    
+def times():
+    var = time.localtime()
+    i = 0
+    sec = var.tm_sec
+    min = var.tm_min
+    hour = var.tm_hour
+    day = var.tm_mday 
+  
+    while i < 1000000:
+        i += 1
+        if sec >= 59:
+            sec = 0
+            if min >= 59:
+                min = 0
+                os.system('clear')
+                print(hour,":", min, ":", sec)
+                if hour > 24:
+                    hour = 0
+                    day += 1
+                    os.system('clear')
+                    print(hour,":", min, ":", sec)
+                else: 
+                    hour += 1
+            else:
+                min += 1
+                os.system('clear')
+                print(hour,":", min, ":", sec)
+        else:
+            sec += 1
+            os.system('clear')
+            print(hour,":", min, ":", sec)
+            time.sleep(0.62)
+            
+
+if __name__ == "__main__":
+    main()
